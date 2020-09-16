bba27bd7293245762236353d891735d85c532790
g0ldbErg <71227180+g0ldbErg@users.noreply.github.com>
2020-09-15 21:20:01 -0300

    static_local/

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -127,3 +127,4 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
